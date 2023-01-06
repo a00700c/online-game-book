594a63fde35aa6ccb237d87d88c7467622d28341
a00700c <a00700c@gmail.com>
2023-01-06 20:53:03 +0900

    Initial commit

---
+++ src/test/java/gamebook/gamebook/GamebookApplicationTests.java
@@ -0,0 +1,13 @@
+package gamebook.gamebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
