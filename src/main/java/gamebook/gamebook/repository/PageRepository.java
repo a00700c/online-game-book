3e6eb6898c88ad45f415a689d6e70f7880d57539
a00700c <a00700c@gmail.com>
2023-01-09 22:22:31 +0900

    [ADD] page search 메서드 추가 및 테스트 코드 작성

--- src/main/java/gamebook/gamebook/repository/PageRepository.java
+++ src/main/java/gamebook/gamebook/repository/PageRepository.java
@@ -2,6 +2,9 @@ package gamebook.gamebook.repository;
 
 import gamebook.gamebook.entity.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PageRepository extends JpaRepository<Page, Long> {
+
+    List<Page> findAllByGamebookGbNumOrderByPageNumAsc(Long gbNum);
 }
---
+++ src/test/java/gamebook/gamebook/repository/PageRepositoryTest.java
@@ -0,0 +1,56 @@
+package gamebook.gamebook.repository;
+
+import gamebook.gamebook.entity.Gamebook;
+import gamebook.gamebook.entity.Member;
+import gamebook.gamebook.entity.Page;
+import gamebook.gamebook.service.GamebookService;
+import gamebook.gamebook.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+@Slf4j
+public class PageRepositoryTest {
+
+    @Autowired PageRepository pageRepository;
+    @Autowired MemberService memberService;
+    @Autowired GamebookService gamebookService;
+
+    @Test
+    @Rollback(value = false)
+    public void pageRepositoryTest() {
+        Member member = new Member();
+        member.initMember("member1", "1234", "guy");
+        memberService.join(member);
+
+        Member member2 = new Member();
+        member2.initMember("member2", "1234", "auy");
+        memberService.join(member2);
+
+        Long findNum = gamebookService.makeNewGamebook("gb1", "aaa", "member1");
+        Long findNum2 = gamebookService.makeNewGamebook("gb2", "aaa", "member2");
+        Gamebook gamebook = gamebookService.findByGbNum(findNum);
+        Gamebook gamebook2 = gamebookService.findByGbNum(findNum2);
+        Page page = Page.createPage(gamebook);
+        Page page2 = Page.createPage(gamebook);
+        Page page3 = Page.createPage(gamebook2);
+        pageRepository.save(page);
+        pageRepository.save(page2);
+        pageRepository.save(page3);
+
+        List<Page> findPages = pageRepository.findAllByGamebookGbNumOrderByPageNumAsc(1L);
+        findPages.forEach(s ->
+                log.info("page id = {}, gb_num = {}", s.getPageId(), s.getGamebook().getGbNum()));
+
+    }
+}
