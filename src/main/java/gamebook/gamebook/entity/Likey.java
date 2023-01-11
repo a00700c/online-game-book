bc671d660b1c50509ab3dce7721a116d34e84d79
a00700c <a00700c@gmail.com>
2023-01-11 20:29:45 +0900

    [ADD] likey 엔티티 및 repository 작성

--- src/main/java/gamebook/gamebook/entity/Likey.java
+++ src/main/java/gamebook/gamebook/entity/Likey.java
@@ -18,4 +18,21 @@ public class Likey {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "gb_num")
     private Gamebook gamebook;
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getLikeys().add(this);
+    }
+
+    public void setGamebook(Gamebook gamebook) {
+        this.gamebook = gamebook;
+        gamebook.getLikeys().add(this);
+    }
+
+    public static Likey createLikey(Member member, Gamebook gamebook) {
+        Likey likey = new Likey();
+        likey.setMember(member);
+        likey.setGamebook(gamebook);
+        return likey;
+    }
 }
--- src/main/java/gamebook/gamebook/repository/LikeyRepository.java
+++ src/main/java/gamebook/gamebook/repository/LikeyRepository.java
@@ -3,5 +3,9 @@ package gamebook.gamebook.repository;
 import gamebook.gamebook.entity.Likey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeyRepository extends JpaRepository<Likey, Long> {
+
+    List<Likey> findAllByMemberIdOrderByIdDesc(Long memberId);
 }
