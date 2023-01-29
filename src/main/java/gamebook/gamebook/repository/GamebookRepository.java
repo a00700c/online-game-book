b025065a86353c319fa8849dd126d682613aaee1
a00700c <a00700c@gmail.com>
2023-01-29 20:15:27 +0900

    [UPDATE] 랭킹 조회 쿼리 최적화

--- src/main/java/gamebook/gamebook/repository/GamebookRepository.java
+++ src/main/java/gamebook/gamebook/repository/GamebookRepository.java
@@ -2,6 +2,7 @@ package gamebook.gamebook.repository;
 
 import gamebook.gamebook.entity.Gamebook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,5 +14,6 @@ public interface GamebookRepository extends JpaRepository<Gamebook, Long> {
 
     List<Gamebook> findAllByOrderByGbNumDesc();
 
+    @Query("SELECT distinct g from Gamebook g join fetch g.member")
     List<Gamebook> findAllByOrderByLikeNumDesc();
 }
