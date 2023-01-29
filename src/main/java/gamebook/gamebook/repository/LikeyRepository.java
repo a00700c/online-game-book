0c1693e533a3a921214f8d2a7a65ac367312c140
a00700c <a00700c@gmail.com>
2023-01-29 20:22:40 +0900

    [UPDATE] 좋아요 목록 검색 쿼리 최적화

--- src/main/java/gamebook/gamebook/repository/LikeyRepository.java
+++ src/main/java/gamebook/gamebook/repository/LikeyRepository.java
@@ -2,12 +2,14 @@ package gamebook.gamebook.repository;
 
 import gamebook.gamebook.entity.Likey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface LikeyRepository extends JpaRepository<Likey, Long> {
 
+    @Query("SELECT distinct l from Likey l join fetch l.gamebook join fetch l.member")
     List<Likey> findAllByMemberIdOrderByIdDesc(String memberId);
 
     Optional<Likey> findByMemberIdAndGamebookGbNum(String memberId, Long gbNum);
